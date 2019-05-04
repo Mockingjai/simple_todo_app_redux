73b14c006858eba808a22a49757469380d98b924
Illya Velichko <bitbetmy@gmail.com>
2019-05-04 20:55:17 +0300

    Initial commit

---
+++ src/app-component/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app_wrapper">
+                <div className="app_wrapper-content">
+                    <div className="title_wrapper">
+                        <h3>Todo <span>A</span>pp with Redux</h3>
+                    </div>
+                    <div className="todoInput_wrapper-App">
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
